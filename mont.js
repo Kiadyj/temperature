f8f58b18606b4b9810a130697ee7e8917c836b4f
Kiadyj <99488289+Kiadyj@users.noreply.github.com>
2022-03-18 04:49:44 +0300

    Update mont.js

--- mont.js
+++ mont.js
@@ -1,9 +1,10 @@
 // Déclarer le tableau
 let tableau = [1,2,3,4,10,11]
 
-tableau.sort((a, b) => a - b)
+tableau.sort((a, b) => a - b)//sorts array elements
 
 console.log(tableau);
+//returns the value of the first element found in the array that meets the given condition
 let find = (number, tableau) => {
     if(tableau == []){
         return false
